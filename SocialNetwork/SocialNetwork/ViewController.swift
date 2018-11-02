dbb2b4648336457b6179130f13d4c7c967e2dde4
viktor <viktorzp84@gmail.com>
2018-11-02 16:03:31 +0200

    start point

---
+++ SocialNetwork/SocialNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Viktor Pechersky on 02.11.2018.
+//  Copyright © 2018 Viktor Pecherskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
